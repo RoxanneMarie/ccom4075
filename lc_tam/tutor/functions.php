fcc98405c650152826ea18c6db5daa87aaefe513
Roxanne M. Martínez Albino <roxanne.martinez1@upr.edu>
2022-05-01 01:32:29 -0400

    calendar tutor

--- lc_tam/tutor/functions.php
+++ lc_tam/tutor/functions.php
@@ -110,7 +110,44 @@ function top_header_7() //Menu para tutor en la interface de estudiante.
 
         </section>';
 }
+function dep($data)
+ {
+     $format = print_r('<pre>');
+     $format.= print_r($data);
+     $format.= print_r('</pre>');
+     return $format;
+ }
 
 
+function calendar(){
+    
+    $id = $_SESSION['email'];
+  
+     $query = query("SELECT * FROM lc_test_tutors WHERE student_email = '$id'");
+     confirm($query);
+     $row = fetch_array($query);
+     $idTutor = $row['tutor_id'];
+     
+     $query2 = query("SELECT * FROM lc_sessions WHERE tutor_id = ".$idTutor." AND capacity != 8");
+     confirm($query2);
+     $count =0;
+     while ($data = fetch_array($query2)){
+        $arrData [] = $data;
+        $count++;
+      }
+      $data = array();
+            foreach($arrData as $row)
+            {
+            $data[] = array(
+            'id'   => $row["session_id"],
+            'title'   => $row["course_id"],
+            'start'   => $row["session_date"]." ".$row["start_time"],
+            'end'   => $row["session_date"]." ".$row["end_time"],
+            );
+            }
+ 
+        echo json_encode($data);
+        
+     }     
 
 ?>
---
+++ lc_tam/tutor/load.php
@@ -0,0 +1,32 @@
+<?php
+ require_once("../functions.php"); 
+ require_once("functions.php") ;
+ 
+$id = $_SESSION['email'];
+  
+$query = query("SELECT * FROM lc_test_tutors WHERE student_email = '$id'");
+confirm($query);
+$row = fetch_array($query);
+$idTutor = $row['tutor_id'];
+
+$query2 = query("SELECT * FROM lc_sessions WHERE tutor_id = ".$idTutor." AND capacity != 8");
+confirm($query2);
+$count =0;
+while ($data = fetch_array($query2)){
+   $arrData [] = $data;
+   $count++;
+ }
+ $data = array();
+       foreach($arrData as $row)
+       {
+       $data[] = array(
+       'id'   => $row["session_id"],
+       'title'   => $row["course_id"],
+       'start'   => $row["session_date"]." ".$row["start_time"],
+       'end'   => $row["session_date"]." ".$row["end_time"],
+       );
+       }
+
+   echo json_encode($data);   
+
+?>
